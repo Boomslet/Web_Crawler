bb6481f9f1febb9d7e8bca8d19528bac3d60c04c
Mark Boon <Boomslet@users.noreply.github.com>
2017-07-25 22:20:28 +0800

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -6,47 +6,34 @@ Version: -
 """
 
 import lxml
-import os
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
+import util
+
 
 url_list = []
 crawled_list = []
 
 
-def create_dirs(url_name):
-    ind = url_name.find(".") + 1
-    url_name = url_name[ind:]
-    if not os.path.exists(url_name):
-        print('Creating ' + url_name)
-        os.makedirs(url_name)
-
-
-def write_file(path, data):
-    with open(path, 'a') as f:
-        f.write(data)
-
-
-def crawler(base_url, element="body", keyword=None):
-    
+def crawler(base_url, element="body"):
     url_list.append(base_url)
 
-    for url in url_list:
-        
+    for url in url_list:     
         crawled_list.append(url)
         url_list.remove(url)
-
-        create_dirs(url)
         
-        page = requests.get(url).text
-        soup = BeautifulSoup(page, 'lxml')
-        data = soup.find_all(element)
-        write_file("dump.txt", soup.text)
+        try:
+            util.create_dirs(url)
+            page = requests.get(url).text
+            soup = BeautifulSoup(page, 'lxml')
+            data = soup.find_all(element)
+            util.write_file("dump.txt", soup.text)
 
-        for link in BeautifulSoup(page, 'lxml', parse_only=SoupStrainer('a', href=True)):
-            for link in soup.find_all('a'):
-                try:
+            for link in BeautifulSoup(page, 'lxml', parse_only=SoupStrainer('a', href=True)):
+                for link in soup.find_all('a'):
                     uDom = link['href']
-                    url_list.append(url + uDom)
-                except:
-                    pass
+                    url_list.append(uDom)
+        except:
+            
+            pass
+
