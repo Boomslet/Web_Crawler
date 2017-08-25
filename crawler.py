01a4a71fe39cd74b3f0ad41f3d4ad30a23fa2d84
Mark Boon <Boomslet@users.noreply.github.com>
2017-08-26 00:26:10 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -3,8 +3,8 @@ A simple-to-use web worker that collects
 website data as it crawls
 
 Author: Mark Boon
-Date: 25/08/2017
-Version: 2.2.1
+Date: 26/08/2017
+Version: 2.3.0
 """
 
 import threading
@@ -16,6 +16,7 @@ from bs4 import BeautifulSoup
 class Worker:
     base_url = ''
     queue = []
+    crawled = set()
     lock = threading.Semaphore(value=1)
 
     def __init__(self, base_url):
@@ -29,9 +30,9 @@ class Worker:
             f.close()
 
     def report(self, url):
-        self.lock.acquire()
+        #self.lock.acquire()
         print("Successfully crawled", url)
-        self.lock.release()
+        #self.lock.release()
 
     def work(self):
         for link in self.queue:
@@ -43,11 +44,12 @@ class Worker:
                 self.write_file("dump.txt", soup.text)
                 self.write_file("log.txt", link + "\n")
                 self.report(link)
+                self.crawled.add(link)
 
                 for upper in soup.find_all('a', href=True):
-                    dom = urljoin(self.base_url, upper['href'])
-                    if dom not in set(self.queue):
-                        self.queue.append(dom)
+                    joined_link = urljoin(self.base_url, upper['href'])
+                    if joined_link not in self.crawled:
+                        self.queue.append(joined_link)
 
             except:
                 self.write_file("error_log.txt", str(link) + "\n")
