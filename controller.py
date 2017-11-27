8481d81a1ef01be2943b4ea1dc609b774948cafc
Mark Boon <Boomslet@users.noreply.github.com>
2017-11-27 11:31:05 +0800

    Update controller.py

--- controller.py
+++ controller.py
@@ -10,6 +10,7 @@ from crawler import *
 
 
 def crawl(*urls):
+    // load crawler threads
     for link in urls:
         current_worker = Worker(link)
         load = threading.Thread(target=current_worker.work)
